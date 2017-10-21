f111dbfa3451fcfb66cfd4f4814ad3dd4a1a0866
Ubuntu <ubuntu@ip-172-31-37-39.us-west-2.compute.internal>
2017-10-21 02:15:18 +0000

    api address http://54.186.229.227/

--- client/src/utils.js
+++ client/src/utils.js
@@ -5,7 +5,7 @@ import Axios from 'axios';
 
 // const API_ADDRESS = process.env.DEFAULT_AXIOS_ADDRESS || 'http://localhost:2978';
 // const API_ADDRESS = process.env.DEFAULT_AXIOS_ADDRESS || 'http://47.94.99.252:2976';
-const API_ADDRESS = process.env.DEFAULT_AXIOS_ADDRESS || 'http://54.245.25.56:2976';
+const API_ADDRESS = process.env.DEFAULT_AXIOS_ADDRESS || 'http://54.186.229.227:2976';
 
 Axios.defaults.baseURL = API_ADDRESS;
 export { Axios };
