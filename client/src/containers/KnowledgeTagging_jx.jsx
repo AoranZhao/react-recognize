c778a9e237e31a92f91983f021e5f929c9ccb8dd
AoranZhao <aoran_zhao@yahoo.ca>
2018-04-19 14:23:18 -0400

    update

--- client/src/containers/KnowledgeTagging_jx.jsx
+++ client/src/containers/KnowledgeTagging_jx.jsx
@@ -143,7 +143,8 @@ class KTJXPage extends React.Component {
                             <br />
                             <div style={output_style}>
                                 <p>Output: </p>
-                                <pre>{JSON.stringify(this.props.ktjx.outputs, null, 4)}</pre>
+                                <pre>{JSON.parse(this.props.ktjx.outputs)}</pre>
+                                <pre>{JSON.stringify(JSON.parse(this.props.ktjx.outputs, null, 4))}</pre>
                             </div>
                         </div> : <div></div>}
                         <br />
