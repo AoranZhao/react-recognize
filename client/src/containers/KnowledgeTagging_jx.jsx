2a788a35c3c848e0bedb69cbaa2ce774df2c857c
AoranZhao <aoran_zhao@yahoo.ca>
2018-04-19 15:08:40 -0400

    fix

--- client/src/containers/KnowledgeTagging_jx.jsx
+++ client/src/containers/KnowledgeTagging_jx.jsx
@@ -146,7 +146,7 @@ class KTJXPage extends React.Component {
                             <br />
                             <div style={output_style}>
                                 <p>Output: </p>
-                                <pre>{new DOMParser().parseFromString(this.syntaxHighlight(JSON.parse(this.props.ktjx.outputs)))}</pre>
+                                <pre>{new DOMParser().parseFromString(this.syntaxHighlight(JSON.parse(this.props.ktjx.outputs), 'text/xml'))}</pre>
                             </div>
                         </div> : <div></div>}
                         <br />
