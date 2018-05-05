2e1e96121358acad869ca58a3e1f9f31306afc67
AoranZhao <aoran_zhao@yahoo.ca>
2018-05-05 06:59:09 -0400

    update

--- client/src/containers/VideoPage.jsx
+++ client/src/containers/VideoPage.jsx
@@ -156,8 +156,8 @@ class VideoPage extends React.Component {
     }
 
     generate_output() {
-        return (typeof this.props.video.output !== 'undefined' && !!this.props.video.output.toString().match(/^http:\/\//)) ?
-            <div><a>{'download result from <' + this.props.video.output.toString() + '>'}</a></div> : <div></div>
+        return (typeof this.props.video.output !== 'undefined' && typeof this.props.video.output.output !== 'undefined' && !!this.props.video.output.output.toString().match(/^http:\/\//)) ?
+            <div><a>{'download result from <' + this.props.video.output.output.toString() + '>'}</a></div> : <div></div>
     }
 
     render() {
